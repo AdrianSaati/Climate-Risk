4c5066a35ad070dab7868ae13c904a6b88bebce0
ASaati <ad94kaz@gmail.com>
2021-02-17 10:51:49 +0100

    .py update

--- dashboard.py
+++ dashboard.py
@@ -20,6 +20,8 @@ trdf = pd.read_excel('DF.xlsx',sheet_name=1)
 
 # Creating graphs
 
+template = 'plotly_dark'
+
 ## Transition Risk
 
 ### Primary Portfolio Metrics
@@ -33,17 +35,17 @@ df_tr_noOfProperties = df_tr_noOfProperties.to_frame()
 df_tr_noOfProperties.reset_index(level=0,inplace=True)
 df_tr_noOfProperties.rename(columns={'Risk':'Properties'},inplace=True)
 df_tr_noOfProperties.rename(columns={'index':'Risk'},inplace=True)
-tr_numberOfProperties1 = px.pie(df_tr_noOfProperties,values='Properties',names='Risk')
-tr_numberOfProperties2 = px.histogram(trdf,x='Risk')
+tr_numberOfProperties1 = px.pie(df_tr_noOfProperties,values='Properties',names='Risk',template=template, title="Number of Properties")
+tr_numberOfProperties2 = px.histogram(trdf,x='Risk',template=template, title="Number of Properties")
 
 ##### Total outstanding loans
 # creating temp. df with cumulation of outstanding loan for each risk in transition risk
 df_tr_TotalOutstandingLoans = pd.DataFrame(trdf.groupby('Risk')['Outstanding loan'].sum()).reset_index()
 # final graph of total outstanding loans for each risk in transition risk
-tr_TotalOutstandingLoans = px.pie(df_tr_TotalOutstandingLoans,names='Risk',values='Outstanding loan')
+tr_TotalOutstandingLoans = px.pie(df_tr_TotalOutstandingLoans,names='Risk',values='Outstanding loan',template=template, title="Total Outstanding Loans")
 
 ##### Maximum/range of financial impact (e.g. RWA / ECL)
-tr_financialImpact = px.box(trdf, x="Risk", y="ECL", points="all")
+tr_financialImpact = px.box(trdf, x="Risk", y="ECL", points="all",template=template, title="Financial Impact")
 # ECL min values
 df_tr_financialImpact_min = pd.DataFrame(trdf.groupby('Risk')['ECL'].min()).reset_index().rename(columns={'ECL':'min ECL'})
 # ECL max values
@@ -58,9 +60,8 @@ trdf['Control Column'] = cc
 # creating df with summed EPC occurances
 df_distributionOfCurrentEPC = pd.DataFrame(trdf.groupby('EPC')['Control Column'].count()).reset_index()
 # dodać nazwy axis i tytuł
-distributionOfCurrentEPC = px.bar(df_distributionOfCurrentEPC, x="EPC", y='Control Column', title="EPC distribution", category_orders={"EPC": ["A", "B", "C", "D","E","F","G"]}, labels={
-                     "Risk": "Risk",
-                 },)
+distributionOfCurrentEPC = px.bar(df_distributionOfCurrentEPC, x="EPC", y='Control Column', category_orders={"EPC": ["A", "B", "C", "D","E","F","G"]}, 
+                          labels={"Risk": "Risk"},template=template, title="Distribution of Current EPC")
 distributionOfCurrentEPC.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
 
 ### Exposure Impacts
@@ -69,7 +70,7 @@ distributionOfCurrentEPC.update_layout(barmode='stack', xaxis={'categoryorder':'
 
 ##### GBP cost of property refurbishment to align with net zero scenario
 # assumed that "Cost" column is the cost of net zero refurbishment. Creating a graph presenting cost of refurbishment distribution split into scenarios
-costOfPropertyRef = px.histogram(trdf, x='Cost', marginal='rug',nbins=20, color='Scenario')
+costOfPropertyRef = px.histogram(trdf, x='Cost', marginal='rug',nbins=20, color='Scenario',template=template, title="Cost of Property Refurbishment")
 
 ##### Property refurbishment cost as % of property value
 # temp df. with 3 values
@@ -79,7 +80,8 @@ df_refCostAsPercentOfPropertyValue['Cost as percent of property value'] = df_ref
 # final scatter plot rug shows density, size shows property value, y axis shows cost of refurbishment, x axis shows Cost to Property Value ratio, Color shows the scenario
 refCostAsPercentOfPropertyValue = px.scatter(df_refCostAsPercentOfPropertyValue, x="Cost as percent of property value", y='Cost',
 	         size="Property value", color="Scenario",
-          log_x=True, size_max=60, marginal_x = 'rug')
+          log_x=True, size_max=60, marginal_x = 'rug',
+          template=template, title="Refurbishment Cost as Percent of Property Value")
 
 ##### Annualised refurbishment cost as % of mortgage payment
 #formula: Total Refurbishment Cost / (Monthly mortgage payment * 12)
@@ -94,35 +96,60 @@ for i in range(len(list_cost)):
 # add list to temp. df
 df_annualisedRefCostAsPercentOfMortgagePayment['Annual. Ref. Cost as Percent of Mortgage Payment'] = list_annualisedRefCostAsPercOfMortgagePayment
 # final plot
-annualisedRefCostAsPercentOfMortgagePayment = px.histogram(df_annualisedRefCostAsPercentOfMortgagePayment, x="Annual. Ref. Cost as Percent of Mortgage Payment", color='Scenario', marginal='rug' ) 
+annualisedRefCostAsPercentOfMortgagePayment = px.histogram(df_annualisedRefCostAsPercentOfMortgagePayment, x="Annual. Ref. Cost as Percent of Mortgage Payment", color='Scenario', marginal='rug', template=template, title="Annualised Refurbishment Cost as Percent of Mortgage Payment" ) 
 
 ##### Property valuation haircut
-propertyValuationHaircut1 = px.bar(trdf, x="Property valuation haircut", color='Scenario', barmode='group',hover_data=['Property value', 'Property valuation haircut']) 
-propertyValuationHaircut2 = px.scatter(trdf, x="Property valuation haircut", hover_data=['Property value', 'Property valuation haircut','ECL'], color='Scenario',  marginal_x='rug', size='ECL') 
+propertyValuationHaircut1 = px.histogram(trdf, x="Property valuation haircut", color='Scenario', barmode='group',template=template, title="Property Valuation Haircut") 
+propertyValuationHaircut2 = px.scatter(trdf, x="Property valuation haircut", hover_data=['Property value', 'Property valuation haircut','ECL'], color='Scenario',  marginal_x='rug', size='ECL', template=template, title="Property Valuation Haircut") 
 
 #### Cross-scenario customer metrics used in underwriting
 
 ###### Risk rating = High/Med/Low
-# x = trdf.Risk.values.tolist()
-# y = trdf.Scenario.values.tolist()
+x = trdf.Risk.values.tolist()
+y = trdf.Scenario.values.tolist()
 # RiskRating1 = iplot([Histogram2dContour(x=x, y=y, contours=Contours(coloring='heatmap')),
 #        Scatter(x=x, y=y, mode='markers', marker=Marker(color='white', size=1, opacity=0.3))], show_link=False)
+RiskRating1 = px.density_contour(trdf,x='Risk',y='Scenario',template=template, title="Risk Rating")
+RiskRating1.update_traces(contours_coloring="fill", contours_showlabels = True)
 
+###### Maximum Annualised Refurbishment Cost Across Scenarios
+tr_maximumAnnualisedRefCostAcrossScenarios = px.box(trdf,x='Scenario',y='Cost',template=template, title="Maximum Annualised Refurbishment Cost Across Scenarios")
 
-app = dash.Dash()
+####################################
+####################################
+####################################
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# @import url(https://fonts.googleapis.com/css?family=Roboto);
+
+app = dash.Dash(
+      __name__,
+    external_stylesheets=external_stylesheets
+)
 
 app.layout = html.Div([
-    html.H1(children= 'Climate Risk',
+    html.Div(children= 'Climate Risk',
     style={
-      'textAlign':'center',
-      'color':'#A100FF',
+      'textAlign':'left',
+      'color':'#8359A3',
+      'backgroundColor':'#0e0e0e',
+      'font-size':'40px',
+      'font_family': 'Roboto',
+      'opacity':0.6,
+      'padding':'15px',
     }),
 
 
     html.Div(children = 'Transition Risk',
     style={
-      'textAlign':'center',
-      'color':'#A100FF',
+      'textAlign':'left',
+      'color':'#8800FF',
+      'backgroundColor':'#0e0e0e',
+      'font-size':'30px',
+      'font_family': 'Roboto',
+      'opacity':0.9,
+      'padding':'8px',
+      'padding-left':'15px',
     }),
     html.Div(),
 
@@ -135,6 +162,7 @@ app.layout = html.Div([
     dcc.Graph(
         id = 'tr_numberOfProperties1',
         figure = tr_numberOfProperties1,
+      
     ),
     dcc.Graph(
         id = 'tr_numberOfProperties2',
@@ -188,11 +216,17 @@ app.layout = html.Div([
         figure = propertyValuationHaircut2,
     ),
 
-    ##### Risk rating = High/Med/Low
-    # dcc.Graph(
-    #     id = 'RiskRating1',
-    #     figure = RiskRating1,
-    # ),
+    #### Risk rating = High/Med/Low
+    dcc.Graph(
+        id = 'RiskRating1',
+        figure = RiskRating1,
+    ),
+
+    #### Maximum Annualised Refurbishment Cost Across Scenarios
+    dcc.Graph(
+        id = 'tr_maximumAnnualisedRefCostAcrossScenarios',
+        figure = tr_maximumAnnualisedRefCostAcrossScenarios,
+    ),
 
 
 
